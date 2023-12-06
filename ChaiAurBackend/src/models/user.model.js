48502e067084180093d66e5532abe039aea37421
Mr-Mic <bhatade.2114501587@mujonline.edu.in>
2023-12-06 19:33:17 +0530

    add User and Video model

---
+++ ChaiAurBackend/src/models/user.model.js
@@ -0,0 +1,101 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+
+    avatar: {
+      type: String, // Cloudnary URL
+      required: true,
+    },
+
+    coverImage: {
+      type: String, // Cloudnary URL
+    },
+
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+
+    requestToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// hooks use before when user save the data
+userSchema.pre("save", async function (next) {
+  if (!this.isModifed("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// to check pasword is correct or not  using custom methods , its return true / flase
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      // payload_name : ...... coming from database
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIARY,
+    }
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      // payload_name : ...... coming from database
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIARY,
+    }
+  );
+};
+
+export default User = mongoose.model("User", userSchema);
