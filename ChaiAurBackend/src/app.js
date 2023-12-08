477bf820f6b1dc6580c1916ae95ebd4c4bbbe196
Mr-Mic <bhatade.2114501587@mujonline.edu.in>
2023-12-08 15:49:38 +0530

    All setup configured

--- ChaiAurBackend/src/app.js
+++ ChaiAurBackend/src/app.js
@@ -12,9 +12,16 @@ app.use(
 );
 
 // Main configuration files according to express
-app.use(express.json({ linit: "16kb" }));
+app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// Routes import
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
+
+// http://localhost:8000/api/v1/users/register
 export { app };
